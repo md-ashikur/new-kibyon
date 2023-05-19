37ba563af814ed0024283105f5448d8c479c0970
MD Ashikur Rahman <71284265+md-ashikur@users.noreply.github.com>
2023-05-19 16:57:27 +0600

    error checking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#000000",
-          secondary: "#2c71f6",
-          accent: "#37cdbe",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
-        },
-      },
-      
-    ],
-  },
+  plugins: [],
+  
+  
 }
