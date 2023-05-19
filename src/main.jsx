c9802ad85400295067258ed1b5a89bfa3cea5be5
MD Ashikur Rahman <71284265+md-ashikur@users.noreply.github.com>
2023-05-19 09:36:35 +0600

    router error checking

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import { PlayProvider } from "./contexts/Play";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <PlayProvider>
         <App />
       </PlayProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
