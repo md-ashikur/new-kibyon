4857d7dd033b27fb9499fa5812cf41cedcf5e8b6
MD Ashikur Rahman <71284265+md-ashikur@users.noreply.github.com>
2023-05-19 09:02:45 +0600

    page setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  resolve: {
+    alias: [
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+    ],
+  },
   build: {
     chunkSizeWarningLimit: 3000,
     commonjsOptions: {
